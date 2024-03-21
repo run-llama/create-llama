fbe10215bf0d1d99b4f3b8e148c54d612a1c94e0
thucpn <thucsh2@gmail.com>
2024-03-21 11:36:06 +0700

    test: use echo

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@
 npm run clean && ncc build ./index.ts -o ./dist/ --minify --no-cache --no-source-map-register
 
 # add shebang to the top of dist/index.js
-sed -i '1s/^/#!\/usr\/bin\/env node\n\n/' ./dist/index.js
+echo '#!/usr/bin/env node ' | cat - dist/index.js > temp && mv temp dist/index.js
