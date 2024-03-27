16e58ba72df3c4a8d936807514aeda987672909b
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-03-27 08:10:17 +0000

    fix: type interred error for express router

--- templates/types/streaming/express/src/routes/chat.route.ts
+++ templates/types/streaming/express/src/routes/chat.route.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Router } from "express";
 import { chatRequest } from "../controllers/chat-request.controller";
 import { chat } from "../controllers/chat.controller";
 
-const llmRouter = express.Router();
+const llmRouter: Router = express.Router();
 
 llmRouter.route("/").post(chat);
 llmRouter.route("/request").post(chatRequest);
