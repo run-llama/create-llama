06d06673ff4799db4ea022f4c68943067e47802b
leehuwuj <leehuwuj@gmail.com>
2024-04-02 14:16:49 +0700

    add example request

--- templates/types/streaming/fastapi/app/api/routers/chat.py
+++ templates/types/streaming/fastapi/app/api/routers/chat.py
@@ -17,6 +17,18 @@ class _Message(BaseModel):
     role: MessageRole
     content: str
 
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": "How to tune a guitar?",
+                    }
+                ]
+            }
+        }
+
 
 class _ChatData(BaseModel):
     messages: List[_Message]
