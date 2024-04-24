e14e7e99419798d5632fd1fa817d2dfd6c6f1d20
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-04-24 13:08:22 +0700

    add comment to exlain onnxruntime

--- templates/types/streaming/nextjs/webpack.config.mjs
+++ templates/types/streaming/nextjs/webpack.config.mjs
@@ -8,5 +8,14 @@ export default function webpack(config) {
   config.resolve.fallback = {
     aws4: false,
   };
+
+  // Uncomment the following lines when you met issues with onnxruntime-node
+  // The issue can happen when you use onnxruntime-node with pnpm
+  // See: https://github.com/vercel/next.js/issues/43433
+
+  // config.externals.push({
+  //   "onnxruntime-node": "commonjs onnxruntime-node",
+  // });
+
   return config;
 }
