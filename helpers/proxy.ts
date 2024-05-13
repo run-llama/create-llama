87c376be4d9cdf60bc348f8668d6721b02234a4c
Mohammad Amir <amir.zilli@gmail.com>
2024-05-13 13:13:14 +0530

    Formatted code as per the guidelines

--- helpers/proxy.ts
+++ helpers/proxy.ts
@@ -2,7 +2,7 @@
 export async function initializeGlobalAgent() {
   if (process.env.GLOBAL_AGENT_HTTP_PROXY) {
     /* Dynamically import global-agent/bootstrap */
-    require("global-agent/bootstrap");
+    await import("global-agent/bootstrap");
     console.log("Proxy enabled via global-agent.");
   } else {
     console.log("No proxy configuration found. Continuing without proxy.");
