2cab2d898b02ac17ea955c10f9718636e738a0f7
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-05-16 09:42:54 +0700

    add unknown type

--- templates/types/streaming/nextjs/next.config.mjs
+++ templates/types/streaming/nextjs/next.config.mjs
@@ -5,14 +5,4 @@ import webpack from "./webpack.config.mjs";
 const nextConfig = JSON.parse(fs.readFileSync("./next.config.json", "utf-8"));
 nextConfig.webpack = webpack;
 
-// redirect from /data/path-to-file to /api/data/path-to-file
-nextConfig.rewrites = async () => {
-  return [
-    {
-      source: "/data/:path*",
-      destination: "/api/data/:path*",
-    },
-  ];
-};
-
 export default nextConfig;
