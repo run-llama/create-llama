76033e1f0ab8e6906a4cb5d6361d59ce3a70dd46
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-06-06 09:43:49 +0700

    detach card component

--- templates/types/streaming/nextjs/app/components/ui/upload-csv-preview.tsx
+++ templates/types/streaming/nextjs/app/components/ui/upload-csv-preview.tsx
@@ -20,49 +20,59 @@ export interface UploadCsvPreviewProps {
   isNew?: boolean;
 }
 
+function CSVSummaryCard(
+  props: UploadCsvPreviewProps & { fileSizeInKB: number },
+) {
+  const { onRemove, isNew, fileSizeInKB } = props;
+  return (
+    <div className="p-2 w-60 max-w-60 bg-secondary rounded-lg text-sm relative cursor-pointer">
+      <div className="flex flex-row items-center gap-2">
+        <div className="relative h-10 w-10 shrink-0 overflow-hidden rounded-md">
+          <Image
+            className="h-full w-auto"
+            priority
+            src={SheetIcon}
+            alt="SheetIcon"
+          />
+        </div>
+        <div className="overflow-hidden">
+          <div className="truncate font-semibold">
+            {props.csv.filename} ({fileSizeInKB} KB)
+          </div>
+          <div className="truncate text-token-text-tertiary flex items-center gap-2">
+            <span>Spreadsheet</span>
+            {isNew && (
+              <span className="px-2 py-0.5 bg-red-400 text-white text-xs rounded-2xl">
+                new
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+      {onRemove && (
+        <div
+          className={cn(
+            "absolute -top-2 -right-2 w-6 h-6 z-10 bg-gray-500 text-white rounded-full",
+          )}
+        >
+          <XCircleIcon
+            className="w-6 h-6 bg-gray-500 text-white rounded-full"
+            onClick={onRemove}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
 export default function UploadCsvPreview(props: UploadCsvPreviewProps) {
   const { filename, filesize, content } = props.csv;
-  const { onRemove, isNew } = props;
   const fileSizeInKB = Math.round((filesize / 1024) * 10) / 10;
   return (
     <Drawer direction="left">
       <DrawerTrigger asChild>
-        <div className="p-2 w-60 max-w-60 bg-secondary rounded-lg text-sm relative cursor-pointer">
-          <div className="flex flex-row items-center gap-2">
-            <div className="relative h-10 w-10 shrink-0 overflow-hidden rounded-md">
-              <Image
-                className="h-full w-auto"
-                priority
-                src={SheetIcon}
-                alt="SheetIcon"
-              />
-            </div>
-            <div className="overflow-hidden">
-              <div className="truncate font-semibold">
-                {filename} ({fileSizeInKB} KB)
-              </div>
-              <div className="truncate text-token-text-tertiary flex items-center gap-2">
-                <span>Spreadsheet</span>
-                {isNew && (
-                  <span className="px-2 py-0.5 bg-red-400 text-white text-xs rounded-2xl">
-                    new
-                  </span>
-                )}
-              </div>
-            </div>
-          </div>
-          {onRemove && (
-            <div
-              className={cn(
-                "absolute -top-2 -right-2 w-6 h-6 z-10 bg-gray-500 text-white rounded-full",
-              )}
-            >
-              <XCircleIcon
-                className="w-6 h-6 bg-gray-500 text-white rounded-full"
-                onClick={onRemove}
-              />
-            </div>
-          )}
+        <div>
+          <CSVSummaryCard fileSizeInKB={fileSizeInKB} {...props} />
         </div>
       </DrawerTrigger>
       <DrawerContent className="w-3/5 mt-24 h-full max-h-[96%] ">
