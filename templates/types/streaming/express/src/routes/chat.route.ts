d07ffe95e5082cfb700f7b9e3996181fb15f071b
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-06-27 10:52:16 +0700

    feat: add embed api for express

--- templates/types/streaming/express/src/routes/chat.route.ts
+++ templates/types/streaming/express/src/routes/chat.route.ts
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import { chatConfig } from "../controllers/chat-config.controller";
+import { chatEmbed } from "../controllers/chat-embed.controller";
 import { chatRequest } from "../controllers/chat-request.controller";
 import { chat } from "../controllers/chat.controller";
 import { initSettings } from "../controllers/engine/settings";
@@ -10,5 +11,6 @@ initSettings();
 llmRouter.route("/").post(chat);
 llmRouter.route("/request").post(chatRequest);
 llmRouter.route("/config").get(chatConfig);
+llmRouter.route("/embed").post(chatEmbed);
 
 export default llmRouter;
