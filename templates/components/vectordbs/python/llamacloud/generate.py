8328240fe8777e2562af6551d4e3bec0c6e41f6a
Marcus Schiesser <mail@marcusschiesser.de>
2024-06-28 16:25:05 +0200

    fix: python code

--- templates/components/vectordbs/python/llamacloud/generate.py
+++ templates/components/vectordbs/python/llamacloud/generate.py
@@ -22,7 +22,7 @@ def generate_datasource():
     api_key = os.getenv("LLAMA_CLOUD_API_KEY")
     base_url = os.getenv("LLAMA_CLOUD_BASE_URL")
 
-    if not name or not project_name or not api_key:
+    if name is None or project_name is None or api_key is None:
         raise ValueError(
             "Please set LLAMA_CLOUD_INDEX_NAME, LLAMA_CLOUD_PROJECT_NAME and LLAMA_CLOUD_API_KEY"
             " to your environment variables or config them in .env file"
@@ -31,8 +31,8 @@ def generate_datasource():
     documents = get_documents()
 
     LlamaCloudIndex.from_documents(
-        documents,
-        name,
+        documents=documents,
+        name=name,
         project_name=project_name,
         api_key=api_key,
         base_url=base_url,
--- templates/components/vectordbs/python/llamacloud/index.py
+++ templates/components/vectordbs/python/llamacloud/index.py
@@ -1,7 +1,3 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
 import logging
 import os
 from llama_index.indices.managed.llama_cloud import LlamaCloudIndex
@@ -16,14 +12,14 @@ def get_index():
     api_key = os.getenv("LLAMA_CLOUD_API_KEY")
     base_url = os.getenv("LLAMA_CLOUD_BASE_URL")
 
-    if not name or not project_name or not api_key:
+    if name is None or project_name is None or api_key is None:
         raise ValueError(
             "Please set LLAMA_CLOUD_INDEX_NAME, LLAMA_CLOUD_PROJECT_NAME and LLAMA_CLOUD_API_KEY"
             " to your environment variables or config them in .env file"
         )
 
     index = LlamaCloudIndex(
-        name,
+        name=name,
         project_name=project_name,
         api_key=api_key,
         base_url=base_url,
