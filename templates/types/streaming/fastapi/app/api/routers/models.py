3befca493e13756608e13a1f251b7f490040136b
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-07-05 12:12:53 +0700

    feat: get llamacloud file url in fast api

--- templates/types/streaming/fastapi/app/api/routers/models.py
+++ templates/types/streaming/fastapi/app/api/routers/models.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import requests
 from pydantic import BaseModel, Field, validator
 from pydantic.alias_generators import to_camel
 from typing import List, Any, Optional, Dict
@@ -112,6 +113,40 @@ class ChatData(BaseModel):
         return self.messages[-1].role == MessageRole.USER
 
 
+class LLamaCloudFileService(BaseModel):
+    @classmethod
+    def get_files(cls, pipeline_id: str) -> List[Dict[str, Any]]:
+        url = f"https://cloud.llamaindex.ai/api/v1/pipelines/{pipeline_id}/files"
+        payload = {}
+        headers = {
+            "Accept": "application/json",
+            "Authorization": f'Bearer {os.getenv("LLAMA_CLOUD_API_KEY")}',
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+
+    @classmethod
+    def get_file_detail(cls, project_id: str, file_id: str) -> str:
+        url = f"https://cloud.llamaindex.ai/api/v1/files/{file_id}/content?project_id={project_id}"
+        payload = {}
+        headers = {
+            "Accept": "application/json",
+            "Authorization": f'Bearer {os.getenv("LLAMA_CLOUD_API_KEY")}',
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+
+    @classmethod
+    def get_file_url(cls, name: str, pipeline_id: str) -> str | None:
+        files = cls.get_files(pipeline_id)
+        for file in files:
+            if file["name"] == name:
+                file_id = file["file_id"]
+                project_id = file["project_id"]
+                return cls.get_file_detail(project_id, file_id)["url"]
+        return None
+
+
 class SourceNodes(BaseModel):
     id: str
     metadata: Dict[str, Any]
@@ -123,6 +158,12 @@ class SourceNodes(BaseModel):
     def from_source_node(cls, source_node: NodeWithScore):
         metadata = source_node.node.metadata
         url = metadata.get("URL")
+
+        # if metadata has pipeline_id, get file url from LLamaCloudFileService
+        pipeline_id = metadata.get("pipeline_id")
+        if pipeline_id:
+            file_name = metadata.get("file_name")
+            url = LLamaCloudFileService.get_file_url(file_name, pipeline_id)
 
         if not url:
             file_name = metadata.get("file_name")
