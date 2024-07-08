608a338245dfce83a205a6c593c05afdc95e189f
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-07-08 19:12:35 +0700

    feat: save document when uploading document

--- templates/types/streaming/express/src/controllers/embeddings.ts
+++ templates/types/streaming/express/src/controllers/embeddings.ts
@@ -1,28 +1,39 @@
+import fs from "fs";
 import {
   Document,
   IngestionPipeline,
-  MetadataMode,
   Settings,
   SimpleNodeParser,
-  TextNode,
 } from "llamaindex";
 import { DocxReader } from "llamaindex/readers/DocxReader";
 import { PDFReader } from "llamaindex/readers/PDFReader";
 import { TextFileReader } from "llamaindex/readers/TextFileReader";
+import crypto from "node:crypto";
 
-export async function readAndSplitDocument(
-  raw: string,
-): Promise<Pick<TextNode, "text" | "embedding">[]> {
+const MIME_TYPE_TO_EXT: Record<string, string> = {
+  "application/pdf": "pdf",
+  "text/plain": "txt",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+    "docx",
+};
+
+export async function uploadDocument(raw: string): Promise<string[]> {
   const [header, content] = raw.split(",");
   const mimeType = header.replace("data:", "").replace(";base64", "");
   const fileBuffer = Buffer.from(content, "base64");
   const documents = await loadDocuments(fileBuffer, mimeType);
+  await saveDocument(fileBuffer, mimeType);
   return await runPipeline(documents);
 }
 
-async function runPipeline(
-  documents: Document[],
-): Promise<Pick<TextNode, "text" | "embedding">[]> {
+async function runPipeline(documents: Document[]): Promise<string[]> {
+  // mark documents to add to the vector store as private
+  for (const document of documents) {
+    document.metadata = {
+      ...document.metadata,
+      private: true,
+    };
+  }
   const pipeline = new IngestionPipeline({
     transformations: [
       new SimpleNodeParser({
@@ -31,13 +42,12 @@ async function runPipeline(
       }),
       Settings.embedModel,
     ],
+    // TODO: make sure this adds the nodes to the vector DB
+    // vectorStore: Settings.vectorStore,
   });
-  const nodes = await pipeline.run({ documents });
-  // remove metadata from text nodes to reduce data send over the wire
-  return nodes.map((node) => ({
-    text: node.getContent(MetadataMode.NONE),
-    embedding: node.embedding,
-  }));
+  await pipeline.run({ documents });
+  // return document identifiers
+  return documents.map((document) => document.id_);
 }
 
 async function loadDocuments(fileBuffer: Buffer, mimeType: string) {
@@ -55,4 +65,26 @@ async function loadDocuments(fileBuffer: Buffer, mimeType: string) {
     default:
       throw new Error(`Unsupported document type: ${mimeType}`);
   }
+}
+
+async function saveDocument(fileBuffer: Buffer, mimeType: string) {
+  const fileExt = MIME_TYPE_TO_EXT[mimeType];
+  if (!fileExt) throw new Error(`Unsupported document type: ${mimeType}`);
+
+  const folder = "data/private";
+  const filename = `${crypto.randomUUID()}.${fileExt}`;
+  const filepath = `${folder}/${filename}`;
+  const fileurl = `${process.env.FILESERVER_URL_PREFIX}/${filepath}`;
+
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder, { recursive: true });
+  }
+  await fs.promises.writeFile(filepath, fileBuffer);
+
+  console.log(`Saved document to ${filepath}. URL: ${fileurl}`);
+  return {
+    filename,
+    filepath,
+    fileurl,
+  };
 }
