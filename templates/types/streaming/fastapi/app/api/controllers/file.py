709ef1f3f788865dac24328e109b8d190b5135a2
leehuwuj <leehuwuj@gmail.com>
2024-07-09 13:11:51 +0700

    use mimetypes lib and change private file folder

--- templates/types/streaming/fastapi/app/api/controllers/file.py
+++ templates/types/streaming/fastapi/app/api/controllers/file.py
@@ -1,6 +1,7 @@
 import os
 import base64
 import tempfile
+import mimetypes
 from typing import List, Dict
 from pathlib import Path
 from llama_index.core import VectorStoreIndex
@@ -11,72 +12,6 @@ from llama_index.core.readers.file.base import (
 from llama_index.core.ingestion import IngestionPipeline
 from app.engine.index import get_index
 
-MINE_TYPE_EXTENSION_MAP = {
-    "application/pdf": "pdf",
-    "application/x-t602": "602",
-    "application/x-abiword": "zabw",
-    "image/cgm": "cgm",
-    "application/x-cwk": "cwk",
-    "application/msword": "dot",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "docx",
-    "application/vnd.ms-word.document.macroEnabled.12": "docm",
-    "application/vnd.ms-word.template.macroEnabled.12": "dotm",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.template": "dotx",
-    "application/x-hwp": "hwp",
-    "application/x-iwork-keynote-sffkey": "key",
-    "application/vnd.lotus-wordpro": "lwp",
-    "application/macwriteii": "mcw",
-    "application/x-iwork-pages-sffpages": "pages",
-    "application/x-pagemaker": "pbd",
-    "application/vnd.ms-powerpoint": "pot",
-    "application/vnd.ms-powerpoint.presentation.macroEnabled.12": "pptm",
-    "application/vnd.openxmlformats-officedocument.presentationml.presentation": "pptx",
-    "application/vnd.ms-powerpoint.template.macroEnabled.12": "potm",
-    "application/vnd.openxmlformats-officedocument.presentationml.template": "potx",
-    "application/rtf": "rtf",
-    "application/vnd.stardivision.draw": "sda",
-    "application/vnd.stardivision.impress": "sdd",
-    "application/sdp": "sdp",
-    "application/vnd.stardivision.writer": "vor",
-    "application/vnd.sun.xml.impress.template": "sti",
-    "application/vnd.sun.xml.impress": "sxi",
-    "application/vnd.sun.xml.writer": "sxw",
-    "application/vnd.sun.xml.writer.template": "stw",
-    "application/vnd.sun.xml.writer.global": "sxg",
-    "text/plain": "txt",
-    "application/vnd.uoml+xml": "uos2",
-    "application/vnd.openofficeorg.presentation": "uop",
-    "application/x-uo": "uot",
-    "application/wordperfect": "wpd",
-    "application/vnd.ms-works": "xlr",
-    "application/xml": "xml",
-    "application/epub+zip": "epub",
-    "image/jpeg": "jpeg",
-    "image/png": "png",
-    "image/gif": "gif",
-    "image/bmp": "bmp",
-    "image/svg+xml": "svg",
-    "image/tiff": "tiff",
-    "image/webp": "webp",
-    "text/html": "html",
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "xlsx",
-    "application/vnd.ms-excel": "et",
-    "application/vnd.ms-excel.sheet.macroEnabled.12": "xlsm",
-    "application/vnd.ms-excel.sheet.binary.macroEnabled.12": "xlsb",
-    "text/csv": "csv",
-    "application/x-dif": "dif",
-    "text/vnd.sylk": "slk",
-    "application/x-prn": "prn",
-    "application/x-iwork-numbers-sffnumbers": "numbers",
-    "application/vnd.oasis.opendocument.spreadsheet": "fods",
-    "application/vnd.dbf": "dbf",
-    "application/vnd.lotus-1-2-3": "123",
-    "application/x-lotus": "wq2",
-    "application/x-quattro-pro": "qpw",
-    "application/ethos": "eth",
-    "text/tab-separated-values": "tsv",
-}
-
 
 def file_metadata_func(*args, **kwargs) -> Dict:
     default_meta = default_file_metadata_func(*args, **kwargs)
@@ -86,27 +21,31 @@ def file_metadata_func(*args, **kwargs) -> Dict:
 
 class FileController:
 
+    PRIVATE_STORE_PATH="private-data"
+
     @staticmethod
     def preprocess_base64_file(base64_content: str) -> tuple:
         header, data = base64_content.split(",", 1)
         mine_type = header.split(";")[0].split(":", 1)[1]
-        extension = MINE_TYPE_EXTENSION_MAP.get(mine_type)
+        extension = mimetypes.guess_extension(mine_type)
         # File data as bytes
         data = base64.b64decode(data)
         return data, extension
 
     @staticmethod
-    def store_and_parse_file(file_data, extension) -> List[str]:
-        # Store file to `data/private` directory
-        os.makedirs("data/private", exist_ok=True)
+    def store_and_parse_file(file_data, extension) -> List["Documents"]:
+        # Store file to the private directory
+        os.makedirs(FileController.PRIVATE_STORE_PATH, exist_ok=True)
 
         with tempfile.NamedTemporaryFile(
-            suffix=f".{extension}", delete=False, dir="data/private"
+            suffix=extension, delete=False, dir=FileController.PRIVATE_STORE_PATH
         ) as temp_file:
             temp_file.write(file_data)
 
             # Read the file
-            reader_cls = get_file_loaders_map().get(f".{extension}")
+            reader_cls = get_file_loaders_map().get(extension)
+            if reader_cls is None:
+                raise ValueError(f"File extension {extension} is not supported")
             documents = reader_cls().load_data(temp_file.name)
             # Add custom metadata
             for doc in documents:
