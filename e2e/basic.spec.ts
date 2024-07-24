25daf36528cc283037f774ff566d284f8159efb9
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-07-24 17:08:58 +0700

    test: observe playwright CI

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -12,13 +12,9 @@ import type {
 import { createTestDir, runCreateLlama, type AppType } from "./utils";
 
 const templateTypes: TemplateType[] = ["streaming"];
-const templateFrameworks: TemplateFramework[] = [
-  "nextjs",
-  "express",
-  "fastapi",
-];
-const dataSources: string[] = ["--no-files", "--example-file", "--llamacloud"];
-const templateUIs: TemplateUI[] = ["shadcn", "html"];
+const templateFrameworks: TemplateFramework[] = ["nextjs"];
+const dataSources: string[] = ["--llamacloud"];
+const templateUIs: TemplateUI[] = ["shadcn"];
 const templatePostInstallActions: TemplatePostInstallAction[] = [
   "none",
   "runApp",
