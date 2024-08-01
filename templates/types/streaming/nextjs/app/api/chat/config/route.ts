39282dc8551f649ce82d44c1dd0c17c3ad5e7898
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-08-01 09:31:54 +0700

    feat: update chat config API to response llamacloud config

--- templates/types/streaming/nextjs/app/api/chat/config/route.ts
+++ templates/types/streaming/nextjs/app/api/chat/config/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { LLamaCloudFileService } from "../llamaindex/streaming/service";
 
 /**
  * This API is to get config from the backend envs and expose them to the frontend
@@ -6,6 +7,15 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const config = {
     starterQuestions: process.env.CONVERSATION_STARTERS?.trim().split("\n"),
+    llamaCloud: await getLLamaCloudConfig(),
   };
   return NextResponse.json(config, { status: 200 });
+}
+
+async function getLLamaCloudConfig() {
+  if (!process.env.LLAMA_CLOUD_API_KEY) return undefined;
+  const projects = await LLamaCloudFileService.getAllProjectsAndPipelines();
+  return {
+    projects,
+  };
 }
