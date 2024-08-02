1014f34b9c384fcc0ba05422df96d3de905c15b7
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-08-02 17:15:51 +0700

    docs: message

--- templates/components/vectordbs/typescript/llamacloud/index.ts
+++ templates/components/vectordbs/typescript/llamacloud/index.ts
@@ -1,13 +1,20 @@
 import { LlamaCloudIndex } from "llamaindex/cloud/LlamaCloudIndex";
 
-export async function getDataSource(params?: any) {
+type LlamaCloudDataSourceParams = {
+  llamaCloudPipeline?: {
+    project: string;
+    pipeline: string;
+  };
+};
+
+export async function getDataSource(params?: LlamaCloudDataSourceParams) {
   const { project, pipeline } = params?.llamaCloudPipeline ?? {};
   const projectName = project ?? process.env.LLAMA_CLOUD_PROJECT_NAME;
   const pipelineName = pipeline ?? process.env.LLAMA_CLOUD_INDEX_NAME;
   const apiKey = process.env.LLAMA_CLOUD_API_KEY;
   if (!projectName || !pipelineName || !apiKey) {
     throw new Error(
-      "Set project, pipeline, and api key in the config file or as environment variables.",
+      "Set project, pipeline, and api key in the params or as environment variables.",
     );
   }
   const index = new LlamaCloudIndex({
