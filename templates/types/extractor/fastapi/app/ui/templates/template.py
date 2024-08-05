ab392c08f2ed80b5e1c59c94a20a6070dc237b59
leehuwuj <leehuwuj@gmail.com>
2024-08-05 18:19:14 +0700

    refactor: remove menu_item_link function from template.py

--- templates/types/extractor/fastapi/app/ui/templates/template.py
+++ templates/types/extractor/fastapi/app/ui/templates/template.py
@@ -17,21 +17,6 @@ default_meta = [
 ]
 
 
-def menu_item_link(text, href):
-    return rx.menu.item(
-        rx.link(
-            text,
-            href=href,
-            width="100%",
-            color="inherit",
-        ),
-        _hover={
-            "color": styles.accent_color,
-            "background_color": styles.accent_text_color,
-        },
-    )
-
-
 class ThemeState(rx.State):
     """The state for the theme of the app."""
 
