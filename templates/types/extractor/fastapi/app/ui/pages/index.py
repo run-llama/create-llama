20cda0272a680429ac7fbd06e590347bb11875cc
leehuwuj <leehuwuj@gmail.com>
2024-08-07 13:47:21 +0700

    add title to extractor app

--- templates/types/extractor/fastapi/app/ui/pages/index.py
+++ templates/types/extractor/fastapi/app/ui/pages/index.py
@@ -12,25 +12,40 @@ from ..templates import template
 
 @template(
     route="/",
-    title="Built by LlamaIndex",
+    title="Structure extractor",
     on_load=[
         SchemaState.init_schema,
         UploadedFilesState.load_files,
-    ]
+    ],
 )
 def index() -> rx.Component:
     """The main index page."""
-    return rx.stack(
+    return rx.vstack(
         rx.vstack(
-            upload_component(),
-            rx.divider(),
-            schema_editor_component(),
-            width="50%",
+            rx.heading("Built by LlamaIndex", size="6"),
+            rx.text(
+                "Upload a file then enter the query to extract the data in the file according to the schema."
+            ),
+            background_color="var(--gray-3)",
+            align_items="left",
+            justify_content="left",
+            width="100%",
+            padding="1rem",
         ),
-        rx.divider(orientation="vertical"),
         rx.stack(
-            extract_data_component(),
-            width="50%",
+            rx.vstack(
+                upload_component(),
+                rx.divider(),
+                schema_editor_component(),
+                width="50%",
+            ),
+            rx.divider(orientation="vertical"),
+            rx.stack(
+                extract_data_component(),
+                width="50%",
+            ),
+            width="100%",
+            padding="1rem",
         ),
         width="100%",
     )
