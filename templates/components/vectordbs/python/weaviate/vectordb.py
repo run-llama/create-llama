0e9b6f18e42f144cf315f8cad9a4e0a7ce4af9e8
leehuwuj <leehuwuj@gmail.com>
2024-08-12 15:43:32 +0700

    simpler global variable for singleton

--- templates/components/vectordbs/python/weaviate/vectordb.py
+++ templates/components/vectordbs/python/weaviate/vectordb.py
@@ -6,38 +6,26 @@ from llama_index.vector_stores.weaviate import WeaviateVectorStore
 DEFAULT_INDEX_NAME = "LlamaIndex"
 
 
-class WeaviateClientSingleton:
-    _instance = None
+def _create_weaviate_client():
+    cluster_url = os.getenv("WEAVIATE_CLUSTER_URL")
+    api_key = os.getenv("WEAVIATE_API_KEY")
+    if not cluster_url or not api_key:
+        raise ValueError(
+            "Environment variables: WEAVIATE_CLUSTER_URL and WEAVIATE_API_KEY are required."
+        )
+    auth_credentials = weaviate.auth.AuthApiKey(api_key)
+    client = weaviate.connect_to_weaviate_cloud(cluster_url, auth_credentials)
+    return client
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(WeaviateClientSingleton, cls).__new__(cls)
-            cls._instance.client = cls._create_weaviate_client()
-        return cls._instance
-
-    @staticmethod
-    def _create_weaviate_client():
-        cluster_url = os.getenv("WEAVIATE_CLUSTER_URL")
-        api_key = os.getenv("WEAVIATE_API_KEY")
-        if not cluster_url or not api_key:
-            raise ValueError(
-                "Environment variables: WEAVIATE_CLUSTER_URL and WEAVIATE_API_KEY are required."
-            )
-        auth_credentials = weaviate.auth.AuthApiKey(api_key)
-        client = weaviate.connect_to_weaviate_cloud(cluster_url, auth_credentials)
-        return client
-
-    def get_client(self):
-        return self.client
-
-    def close(self):
-        self.client.close()
-        WeaviateClientSingleton._instance = None
-
+# Global variable to store the Weaviate client
+client = None
 
 def get_vector_store():
+    global client
+    if client is None:
+        client = _create_weaviate_client()
+
     index_name = os.getenv("WEAVIATE_INDEX_NAME", DEFAULT_INDEX_NAME)
-    client = WeaviateClientSingleton().get_client()
     vector_store = WeaviateVectorStore(
         weaviate_client=client,
         index_name=index_name,
