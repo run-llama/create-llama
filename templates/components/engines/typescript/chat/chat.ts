a29ddb8c3e5018f87a49eeaf9ac9d9d0349b84f5
leehuwuj <leehuwuj@gmail.com>
2024-08-12 15:56:10 +0700

    refactor: Move generateFilters function to separate files

--- templates/components/engines/typescript/chat/chat.ts
+++ templates/components/engines/typescript/chat/chat.ts
@@ -1,10 +1,6 @@
-import {
-  ContextChatEngine,
-  MetadataFilter,
-  MetadataFilters,
-  Settings,
-} from "llamaindex";
+import { ContextChatEngine, Settings } from "llamaindex";
 import { getDataSource } from "./index";
+import { generateFilters } from "./queryFilter";
 
 export async function createChatEngine(documentIds?: string[], params?: any) {
   const index = await getDataSource(params);
@@ -23,28 +19,4 @@ export async function createChatEngine(documentIds?: string[], params?: any) {
     retriever,
     systemPrompt: process.env.SYSTEM_PROMPT,
   });
-}
-
-export function generateFilters(documentIds: string[]): MetadataFilters {
-  // public documents don't have the "private" field or it's set to "false"
-  const publicDocumentsFilter: MetadataFilter = {
-    key: "private",
-    value: ["true"],
-    operator: "nin",
-  };
-
-  // if no documentIds are provided, only retrieve information from public documents
-  if (!documentIds.length) return { filters: [publicDocumentsFilter] };
-
-  const privateDocumentsFilter: MetadataFilter = {
-    key: "doc_id",
-    value: documentIds,
-    operator: "in",
-  };
-
-  // if documentIds are provided, retrieve information from public and private documents
-  return {
-    filters: [publicDocumentsFilter, privateDocumentsFilter],
-    condition: "or",
-  };
 }
