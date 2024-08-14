66ec075c312d22073de0c89e7745f8b509b2dcd7
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-08-14 17:53:43 +0700

    refactor: move llamacloud logic outside runPipeline

--- templates/components/llamaindex/typescript/documents/upload.ts
+++ templates/components/llamaindex/typescript/documents/upload.ts
@@ -1,26 +1,35 @@
 import { VectorStoreIndex } from "llamaindex";
 import { LlamaCloudIndex } from "llamaindex/cloud/LlamaCloudIndex";
-import { loadDocuments, saveDocument } from "./helper";
+import { LLamaCloudFileService } from "../streaming/service";
+import { storeAndParseFile } from "./helper";
 import { runPipeline } from "./pipeline";
 
 export async function uploadDocument(
   index: VectorStoreIndex | LlamaCloudIndex,
+  filename: string,
   raw: string,
 ): Promise<string[]> {
   const [header, content] = raw.split(",");
   const mimeType = header.replace("data:", "").replace(";base64", "");
   const fileBuffer = Buffer.from(content, "base64");
-  const documents = await loadDocuments(fileBuffer, mimeType);
-  const { filename } = await saveDocument(fileBuffer, mimeType);
 
-  // Update documents with metadata
-  for (const document of documents) {
-    document.metadata = {
-      ...document.metadata,
-      file_name: filename,
-      private: "true", // to separate private uploads from public documents
-    };
+  if (index instanceof LlamaCloudIndex) {
+    // trigger LlamaCloudIndex API to upload the file and run the pipeline
+    // @ts-ignore TODO: implement getProjectId in LITS
+    const projectId = index.getProjectId();
+    // @ts-ignore TODO: make getPipelineId public in LITS
+    const pipelineId = index.getPipelineId();
+    return [
+      await LLamaCloudFileService.addFileToPipeline(
+        projectId,
+        pipelineId,
+        new File([fileBuffer], filename, { type: mimeType }),
+        { private: "true" },
+      ),
+    ];
   }
 
-  return await runPipeline(index, documents);
+  // run the pipeline for other vector store indexes
+  const documents = await storeAndParseFile(fileBuffer, mimeType);
+  return runPipeline(index, documents);
 }
