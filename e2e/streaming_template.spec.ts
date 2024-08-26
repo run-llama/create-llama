e6802b7a4d681fc466060ac5d530d83d0fe7c74b
Marcus Schiesser <mail@marcusschiesser.de>
2024-08-26 11:23:30 +0700

    refactor: e2e tests

--- e2e/streaming_template.spec.ts
+++ e2e/streaming_template.spec.ts
@@ -6,14 +6,10 @@ import path from "path";
 import type {
   TemplateFramework,
   TemplatePostInstallAction,
-  TemplateType,
   TemplateUI,
 } from "../helpers";
 import { createTestDir, runCreateLlama, type AppType } from "./utils";
 
-const templateType: TemplateType | undefined = process.env.TEMPLATE
-  ? (process.env.TEMPLATE as TemplateType)
-  : undefined;
 const templateFramework: TemplateFramework = process.env.FRAMEWORK
   ? (process.env.FRAMEWORK as TemplateFramework)
   : "fastapi";
@@ -30,96 +26,94 @@ const appType: AppType = templateFramework === "nextjs" ? "" : "--frontend";
 const userMessage =
   dataSource !== "--no-files" ? "Physical standard for letters" : "Hello";
 
-if (templateType === "streaming") {
-  test.describe(`Test streaming template ${templateFramework} ${dataSource} ${templateUI} ${appType} ${templatePostInstallAction}`, async () => {
-    let port: number;
-    let externalPort: number;
-    let cwd: string;
-    let name: string;
-    let appProcess: ChildProcess;
-    // Only test without using vector db for now
-    const vectorDb = "none";
+test.describe(`Test streaming template ${templateFramework} ${dataSource} ${templateUI} ${appType} ${templatePostInstallAction}`, async () => {
+  let port: number;
+  let externalPort: number;
+  let cwd: string;
+  let name: string;
+  let appProcess: ChildProcess;
+  // Only test without using vector db for now
+  const vectorDb = "none";
 
-    test.beforeAll(async () => {
-      port = Math.floor(Math.random() * 10000) + 10000;
-      externalPort = port + 1;
-      cwd = await createTestDir();
-      const result = await runCreateLlama(
-        cwd,
-        templateType,
-        templateFramework,
-        dataSource,
-        vectorDb,
-        port,
-        externalPort,
-        templatePostInstallAction,
-        templateUI,
-        appType,
-        llamaCloudProjectName,
-        llamaCloudIndexName,
-      );
-      name = result.projectName;
-      appProcess = result.appProcess;
-    });
-
-    test("App folder should exist", async () => {
-      const dirExists = fs.existsSync(path.join(cwd, name));
-      expect(dirExists).toBeTruthy();
-    });
-    test("Frontend should have a title", async ({ page }) => {
-      test.skip(templatePostInstallAction !== "runApp");
-      await page.goto(`http://localhost:${port}`);
-      await expect(page.getByText("Built by LlamaIndex")).toBeVisible();
-    });
-
-    test("Frontend should be able to submit a message and receive a response", async ({
-      page,
-    }) => {
-      test.skip(templatePostInstallAction !== "runApp");
-      await page.goto(`http://localhost:${port}`);
-      await page.fill("form input", userMessage);
-      const [response] = await Promise.all([
-        page.waitForResponse(
-          (res) => {
-            return res.url().includes("/api/chat") && res.status() === 200;
-          },
-          {
-            timeout: 1000 * 60,
-          },
-        ),
-        page.click("form button[type=submit]"),
-      ]);
-      const text = await response.text();
-      console.log("AI response when submitting message: ", text);
-      expect(response.ok()).toBeTruthy();
-    });
-
-    test("Backend frameworks should response when calling non-streaming chat API", async ({
-      request,
-    }) => {
-      test.skip(templatePostInstallAction !== "runApp");
-      test.skip(templateFramework === "nextjs");
-      const response = await request.post(
-        `http://localhost:${externalPort}/api/chat/request`,
-        {
-          data: {
-            messages: [
-              {
-                role: "user",
-                content: userMessage,
-              },
-            ],
-          },
-        },
-      );
-      const text = await response.text();
-      console.log("AI response when calling API: ", text);
-      expect(response.ok()).toBeTruthy();
-    });
-
-    // clean processes
-    test.afterAll(async () => {
-      appProcess?.kill();
-    });
+  test.beforeAll(async () => {
+    port = Math.floor(Math.random() * 10000) + 10000;
+    externalPort = port + 1;
+    cwd = await createTestDir();
+    const result = await runCreateLlama(
+      cwd,
+      "streaming",
+      templateFramework,
+      dataSource,
+      vectorDb,
+      port,
+      externalPort,
+      templatePostInstallAction,
+      templateUI,
+      appType,
+      llamaCloudProjectName,
+      llamaCloudIndexName,
+    );
+    name = result.projectName;
+    appProcess = result.appProcess;
   });
-}
+
+  test("App folder should exist", async () => {
+    const dirExists = fs.existsSync(path.join(cwd, name));
+    expect(dirExists).toBeTruthy();
+  });
+  test("Frontend should have a title", async ({ page }) => {
+    test.skip(templatePostInstallAction !== "runApp");
+    await page.goto(`http://localhost:${port}`);
+    await expect(page.getByText("Built by LlamaIndex")).toBeVisible();
+  });
+
+  test("Frontend should be able to submit a message and receive a response", async ({
+    page,
+  }) => {
+    test.skip(templatePostInstallAction !== "runApp");
+    await page.goto(`http://localhost:${port}`);
+    await page.fill("form input", userMessage);
+    const [response] = await Promise.all([
+      page.waitForResponse(
+        (res) => {
+          return res.url().includes("/api/chat") && res.status() === 200;
+        },
+        {
+          timeout: 1000 * 60,
+        },
+      ),
+      page.click("form button[type=submit]"),
+    ]);
+    const text = await response.text();
+    console.log("AI response when submitting message: ", text);
+    expect(response.ok()).toBeTruthy();
+  });
+
+  test("Backend frameworks should response when calling non-streaming chat API", async ({
+    request,
+  }) => {
+    test.skip(templatePostInstallAction !== "runApp");
+    test.skip(templateFramework === "nextjs");
+    const response = await request.post(
+      `http://localhost:${externalPort}/api/chat/request`,
+      {
+        data: {
+          messages: [
+            {
+              role: "user",
+              content: userMessage,
+            },
+          ],
+        },
+      },
+    );
+    const text = await response.text();
+    console.log("AI response when calling API: ", text);
+    expect(response.ok()).toBeTruthy();
+  });
+
+  // clean processes
+  test.afterAll(async () => {
+    appProcess?.kill();
+  });
+});
