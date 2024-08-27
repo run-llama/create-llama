8f670a935caa94e4bc36af9d2e70fc1f49e80491
Marcus Schiesser <mail@marcusschiesser.de>
2024-08-27 14:14:17 +0700

    fix: allow relative URL in docs (#259)

--- templates/types/streaming/nextjs/app/components/ui/lib/utils.ts
+++ templates/types/streaming/nextjs/app/components/ui/lib/utils.ts
@@ -4,13 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function isValidUrl(url?: string): boolean {
-  if (!url) return false;
-  try {
-    new URL(url);
-    return true;
-  } catch (_) {
-    return false;
-  }
-}
