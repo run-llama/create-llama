a84e88535f2486f269adae1a4795cc447f32f97b
leehuwuj <leehuwuj@gmail.com>
2024-09-27 21:12:30 +0700

    add tools usage for ts

--- templates/components/multiagent/typescript/workflow/agents.ts
+++ templates/components/multiagent/typescript/workflow/agents.ts
@@ -1,5 +1,8 @@
-import { ChatMessage, QueryEngineTool } from "llamaindex";
+import fs from "fs/promises";
+import { BaseToolWithCall, ChatMessage, QueryEngineTool } from "llamaindex";
+import path from "path";
 import { getDataSource } from "../engine";
+import { createTools } from "../engine/tools/index";
 import { FunctionCallingAgent } from "./single-agent";
 
 const getQueryEngineTool = async () => {
@@ -22,10 +25,37 @@ const getQueryEngineTool = async () => {
   });
 };
 
+const getAvailableTools = async () => {
+  const configFile = path.join("config", "tools.json");
+  let toolConfig: any;
+  const tools: BaseToolWithCall[] = [];
+  try {
+    toolConfig = JSON.parse(await fs.readFile(configFile, "utf8"));
+  } catch (e) {
+    console.info(`Could not read ${configFile} file. Using no tools.`);
+  }
+  if (toolConfig) {
+    tools.push(...(await createTools(toolConfig)));
+  }
+
+  return tools;
+};
+
 export const createResearcher = async (chatHistory: ChatMessage[]) => {
+  const tools = await getAvailableTools();
+  const researcherTools = [await getQueryEngineTool()];
+  // Add wikipedia, duckduckgo if they are available
+  for (const tool of tools) {
+    if (
+      tool.metadata.name === "wikipedia.WikipediaToolSpec" ||
+      tool.metadata.name === "duckduckgo_search"
+    ) {
+      researcherTools.push(tool as any);
+    }
+  }
   return new FunctionCallingAgent({
     name: "researcher",
-    tools: [await getQueryEngineTool()],
+    tools: researcherTools,
     systemPrompt:
       "You are a researcher agent. You are given a researching task. You must use your tools to complete the research.",
     chatHistory,
@@ -46,6 +76,23 @@ export const createReviewer = (chatHistory: ChatMessage[]) => {
     name: "reviewer",
     systemPrompt:
       "You are an expert in reviewing blog posts. You are given a task to review a blog post. Review the post for logical inconsistencies, ask critical questions, and provide suggestions for improvement. Furthermore, proofread the post for grammar and spelling errors. Only if the post is good enough for publishing, then you MUST return 'The post is good.'. In all other cases return your review.",
+    chatHistory,
+  });
+};
+
+export const createPublisher = async (chatHistory: ChatMessage[]) => {
+  const tools = await getAvailableTools();
+  const publisherTools: BaseToolWithCall[] = [];
+  for (const tool of tools) {
+    if (tool.metadata.name === "document_generator") {
+      publisherTools.push(tool);
+    }
+  }
+  return new FunctionCallingAgent({
+    name: "publisher",
+    tools: publisherTools,
+    systemPrompt:
+      "You are an expert in publishing blog posts. You are given a task to publish a blog post.",
     chatHistory,
   });
 };
