7c687ac2043d97abc56044bccfc48ba0f643feac
leehuwuj <leehuwuj@gmail.com>
2024-10-02 14:57:33 +0700

    fix missing rename readme in python template

--- helpers/python.ts
+++ helpers/python.ts
@@ -438,6 +438,7 @@ export const installPythonTemplate = async ({
     await copy("**", path.join(root), {
       parents: true,
       cwd: path.join(compPath, "multiagent", "python"),
+      rename: assetRelocator,
     });
   }
 
