2a70f22e1f69a1ca22c0fe21bfb893a407926218
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-10-07 15:58:40 +0700

    fix: import type llamacloud

--- templates/components/vectordbs/typescript/llamacloud/queryFilter.ts
+++ templates/components/vectordbs/typescript/llamacloud/queryFilter.ts
@@ -1,15 +1,17 @@
-import type { MetadataFilter, MetadataFilters } from "@llamaindex/cloud/api";
+import { CloudRetrieveParams, MetadataFilter } from "llamaindex";
 
-export function generateFilters(documentIds: string[]): MetadataFilters {
+export function generateFilters(documentIds: string[]) {
   // public documents don't have the "private" field or it's set to "false"
-  // @ts-ignore TODO: fix typing in @llamaindex/cloud/api
   const publicDocumentsFilter: MetadataFilter = {
     key: "private",
     operator: "is_empty",
   };
 
   // if no documentIds are provided, only retrieve information from public documents
-  if (!documentIds.length) return { filters: [publicDocumentsFilter] };
+  if (!documentIds.length)
+    return {
+      filters: [publicDocumentsFilter],
+    } as CloudRetrieveParams["filters"];
 
   const privateDocumentsFilter: MetadataFilter = {
     key: "file_id", // Note: LLamaCloud uses "file_id" to reference private document ids as "doc_id" is a restricted field in LlamaCloud
@@ -21,5 +23,5 @@ export function generateFilters(documentIds: string[]): MetadataFilters {
   return {
     filters: [publicDocumentsFilter, privateDocumentsFilter],
     condition: "or",
-  };
+  } as CloudRetrieveParams["filters"];
 }
