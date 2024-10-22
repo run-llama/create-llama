8aca022ea4b132be5b9f82f48c3a00dc5e514878
leehuwuj <leehuwuj@gmail.com>
2024-10-22 14:38:27 +0700

    check CI

--- questions/index.ts
+++ questions/index.ts
@@ -1,5 +1,3 @@
-import ciInfo from "ci-info";
-import { getCIQuestionResults } from "./ci";
 import { askProQuestions } from "./questions";
 import { askSimpleQuestions } from "./simple";
 import { QuestionArgs, QuestionResults } from "./types";
@@ -7,9 +5,7 @@ import { QuestionArgs, QuestionResults } from "./types";
 export const askQuestions = async (
   args: QuestionArgs,
 ): Promise<QuestionResults> => {
-  if (ciInfo.isCI) {
-    return await getCIQuestionResults(args);
-  } else if (args.pro) {
+  if (args.pro) {
     // TODO: refactor pro questions to return a result object
     await askProQuestions(args);
     return args as unknown as QuestionResults;
