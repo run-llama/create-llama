0fd6aec8cc57f34f48c050d980bdd9d16769e3f1
Marcus Schiesser <mail@marcusschiesser.de>
2024-10-29 15:10:29 +0700

    added ask models for HF

--- templates/components/settings/python/settings.py
+++ templates/components/settings/python/settings.py
@@ -115,31 +115,6 @@ def init_azure_openai():
     )
 
 
-def init_fastembed():
-    try:
-        from llama_index.embeddings.fastembed import FastEmbedEmbedding
-    except ImportError:
-        raise ImportError(
-            "FastEmbed support is not installed. Please install it with `poetry add llama-index-embeddings-fastembed`"
-        )
-
-    embed_model_map: Dict[str, str] = {
-        # Small and multilingual
-        "all-MiniLM-L6-v2": "sentence-transformers/all-MiniLM-L6-v2",
-        # Large and multilingual
-        "paraphrase-multilingual-mpnet-base-v2": "sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
-    }
-
-    embedding_model = os.getenv("EMBEDDING_MODEL")
-    if embedding_model is None:
-        raise ValueError("EMBEDDING_MODEL environment variable is not set")
-
-    # This will download the model automatically if it is not already downloaded
-    Settings.embed_model = FastEmbedEmbedding(
-        model_name=embed_model_map[embedding_model]
-    )
-
-
 def init_huggingface_embedding():
     try:
         from llama_index.embeddings.huggingface import HuggingFaceEmbedding
