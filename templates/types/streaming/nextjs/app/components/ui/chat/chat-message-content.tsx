57368ea4d216e2b52ab291bcf572c9e48e8df5c7
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-10-31 11:16:54 +0700

    refactor: able to overide markdown

--- templates/types/streaming/nextjs/app/components/ui/chat/chat-message-content.tsx
+++ templates/types/streaming/nextjs/app/components/ui/chat/chat-message-content.tsx
@@ -1,6 +1,7 @@
 import {
   ChatMessage,
   ContentPosition,
+  getSourceAnnotationData,
   useChatMessage,
 } from "@llamaindex/chat-ui";
 import { ToolAnnotations } from "./chat-tools";
@@ -10,11 +11,20 @@ export function ChatMessageContent() {
   const { message } = useChatMessage();
   const customContent = [
     {
+      // override the default markdown component
+      position: ContentPosition.MARKDOWN,
+      component: (
+        <Markdown
+          content={message.content}
+          sources={getSourceAnnotationData(message.annotations)?.[0]}
+        />
+      ),
+    },
+    {
+      // add the tool annotations after events
       position: ContentPosition.AFTER_EVENTS,
       component: <ToolAnnotations message={message} />,
     },
   ];
-  return (
-    <ChatMessage.Content content={customContent} markdownComponent={Markdown} />
-  );
+  return <ChatMessage.Content content={customContent} />;
 }
