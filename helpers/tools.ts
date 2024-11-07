1f9d412d3d1d4db65879f5fd1c3b08877490d5ef
leehuwuj <leehuwuj@gmail.com>
2024-11-07 17:56:17 +0700

    revert wrong change on tools file

--- helpers/tools.ts
+++ helpers/tools.ts
@@ -1,320 +1,361 @@
-import { Context } from "@llamaindex/workflow";
 import fs from "fs/promises";
-import {
-  BaseToolWithCall,
-  callTool,
-  ChatMessage,
-  ChatResponse,
-  ChatResponseChunk,
-  PartialToolCall,
-  QueryEngineTool,
-  ToolCall,
-  ToolCallLLM,
-  ToolCallLLMMessageOptions,
-} from "llamaindex";
-import crypto from "node:crypto";
 import path from "path";
-import { getDataSource } from "../engine";
-import { createTools } from "../engine/tools/index";
-import { AgentRunEvent } from "./type";
+import { red } from "picocolors";
+import yaml from "yaml";
+import { EnvVar } from "./env-variables";
+import { makeDir } from "./make-dir";
+import { TemplateFramework } from "./types";
 
-export const getQueryEngineTool = async (
-  params?: any,
-): Promise<QueryEngineTool | null> => {
-  const topK = process.env.TOP_K ? parseInt(process.env.TOP_K) : undefined;
+export const TOOL_SYSTEM_PROMPT_ENV_VAR = "TOOL_SYSTEM_PROMPT";
 
-  const index = await getDataSource(params);
-  if (!index) {
-    return null;
-  }
-  return new QueryEngineTool({
-    queryEngine: (index as any).asQueryEngine({
-      similarityTopK: topK,
-    }),
-    metadata: {
-      name: "retriever",
-      description: `Use this tool to retrieve information about the provided data.`,
-    },
-  });
+export enum ToolType {
+  LLAMAHUB = "llamahub",
+  LOCAL = "local",
+}
+
+export type Tool = {
+  display: string;
+  name: string;
+  config?: Record<string, any>;
+  dependencies?: ToolDependencies[];
+  supportedFrameworks?: Array<TemplateFramework>;
+  type: ToolType;
+  envVars?: EnvVar[];
 };
 
-export const getAvailableTools = async () => {
-  const configFile = path.join("config", "tools.json");
-  let toolConfig: any;
-  const tools: BaseToolWithCall[] = [];
-  try {
-    toolConfig = JSON.parse(await fs.readFile(configFile, "utf8"));
-  } catch (e) {
-    console.info(`Could not read ${configFile} file. Using no tools.`);
-  }
-  if (toolConfig) {
-    tools.push(...(await createTools(toolConfig)));
-  }
-  const queryEngineTool = await getQueryEngineTool();
-  if (queryEngineTool) {
-    tools.push(queryEngineTool);
-  }
+export type ToolDependencies = {
+  name: string;
+  version?: string;
+};
 
+export const supportedTools: Tool[] = [
+  {
+    display: "Google Search",
+    name: "google.GoogleSearchToolSpec",
+    config: {
+      engine:
+        "Your search engine id, see https://developers.google.com/custom-search/v1/overview#prerequisites",
+      key: "Your search api key",
+      num: 2,
+    },
+    dependencies: [
+      {
+        name: "llama-index-tools-google",
+        version: "^0.2.0",
+      },
+    ],
+    supportedFrameworks: ["fastapi"],
+    type: ToolType.LLAMAHUB,
+    envVars: [
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for google search tool.",
+        value: `You are a Google search agent. You help users to get information from Google search.`,
+      },
+    ],
+  },
+  {
+    // For python app, we will use a local DuckDuckGo search tool (instead of DuckDuckGo search tool in LlamaHub)
+    // to get the same results as the TS app.
+    display: "DuckDuckGo Search",
+    name: "duckduckgo",
+    dependencies: [
+      {
+        name: "duckduckgo-search",
+        version: "6.1.7",
+      },
+    ],
+    supportedFrameworks: ["fastapi", "nextjs", "express"],
+    type: ToolType.LOCAL,
+    envVars: [
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for DuckDuckGo search tool.",
+        value: `You are a DuckDuckGo search agent. 
+You can use the duckduckgo search tool to get information from the web to answer user questions.
+For better results, you can specify the region parameter to get results from a specific region but it's optional.`,
+      },
+    ],
+  },
+  {
+    display: "Wikipedia",
+    name: "wikipedia.WikipediaToolSpec",
+    dependencies: [
+      {
+        name: "llama-index-tools-wikipedia",
+        version: "^0.2.0",
+      },
+    ],
+    supportedFrameworks: ["fastapi", "express", "nextjs"],
+    type: ToolType.LLAMAHUB,
+    envVars: [
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for wiki tool.",
+        value: `You are a Wikipedia agent. You help users to get information from Wikipedia.`,
+      },
+    ],
+  },
+  {
+    display: "Weather",
+    name: "weather",
+    dependencies: [],
+    supportedFrameworks: ["fastapi", "express", "nextjs"],
+    type: ToolType.LOCAL,
+    envVars: [
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for weather tool.",
+        value: `You are a weather forecast agent. You help users to get the weather forecast for a given location.`,
+      },
+    ],
+  },
+  {
+    display: "Document generator",
+    name: "document_generator",
+    supportedFrameworks: ["fastapi", "nextjs", "express"],
+    dependencies: [
+      {
+        name: "xhtml2pdf",
+        version: "^0.2.14",
+      },
+      {
+        name: "markdown",
+        version: "^3.7",
+      },
+    ],
+    type: ToolType.LOCAL,
+    envVars: [
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for document generator tool.",
+        value: `If user request for a report or a post, use document generator tool to create a file and reply with the link to the file.`,
+      },
+    ],
+  },
+  {
+    display: "Code Interpreter",
+    name: "interpreter",
+    dependencies: [
+      {
+        name: "e2b_code_interpreter",
+        version: "0.0.11b38",
+      },
+    ],
+    supportedFrameworks: ["fastapi", "express", "nextjs"],
+    type: ToolType.LOCAL,
+    envVars: [
+      {
+        name: "E2B_API_KEY",
+        description:
+          "E2B_API_KEY key is required to run code interpreter tool. Get it here: https://e2b.dev/docs/getting-started/api-key",
+      },
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for code interpreter tool.",
+        value: `-You are a Python interpreter that can run any python code in a secure environment.
+- The python code runs in a Jupyter notebook. Every time you call the 'interpreter' tool, the python code is executed in a separate cell. 
+- You are given tasks to complete and you run python code to solve them.
+- It's okay to make multiple calls to interpreter tool. If you get an error or the result is not what you expected, you can call the tool again. Don't give up too soon!
+- Plot visualizations using matplotlib or any other visualization library directly in the notebook.
+- You can install any pip package (if it exists) by running a cell with pip install.`,
+      },
+    ],
+  },
+  {
+    display: "Artifact Code Generator",
+    name: "artifact",
+    // Using pre-release version of e2b_code_interpreter
+    // TODO: Update to stable version when 0.0.11 is released
+    dependencies: [
+      {
+        name: "e2b_code_interpreter",
+        version: "0.0.11b38",
+      },
+    ],
+    supportedFrameworks: ["fastapi", "express", "nextjs"],
+    type: ToolType.LOCAL,
+    envVars: [
+      {
+        name: "E2B_API_KEY",
+        description:
+          "E2B_API_KEY key is required to run artifact code generator tool. Get it here: https://e2b.dev/docs/getting-started/api-key",
+      },
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for artifact code generator tool.",
+        value:
+          "You are a code assistant that can generate and execute code using its tools. Don't generate code yourself, use the provided tools instead. Do not show the code or sandbox url in chat, just describe the steps to build the application based on the code that is generated by your tools. Do not describe how to run the code, just the steps to build the application.",
+      },
+    ],
+  },
+  {
+    display: "OpenAPI action",
+    name: "openapi_action.OpenAPIActionToolSpec",
+    dependencies: [
+      {
+        name: "llama-index-tools-openapi",
+        version: "0.2.0",
+      },
+      {
+        name: "jsonschema",
+        version: "^4.22.0",
+      },
+      {
+        name: "llama-index-tools-requests",
+        version: "0.2.0",
+      },
+    ],
+    config: {
+      openapi_uri: "The URL or file path of the OpenAPI schema",
+    },
+    supportedFrameworks: ["fastapi", "express", "nextjs"],
+    type: ToolType.LOCAL,
+    envVars: [
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for openapi action tool.",
+        value:
+          "You are an OpenAPI action agent. You help users to make requests to the provided OpenAPI schema.",
+      },
+    ],
+  },
+  {
+    display: "Image Generator",
+    name: "img_gen",
+    supportedFrameworks: ["fastapi", "express", "nextjs"],
+    type: ToolType.LOCAL,
+    envVars: [
+      {
+        name: "STABILITY_API_KEY",
+        description:
+          "STABILITY_API_KEY key is required to run image generator. Get it here: https://platform.stability.ai/account/keys",
+      },
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for image generator tool.",
+        value: `You are an image generator agent. You help users to generate images using the Stability API.`,
+      },
+    ],
+  },
+  {
+    display: "Azure Code Interpreter",
+    name: "azure_code_interpreter.AzureCodeInterpreterToolSpec",
+    supportedFrameworks: ["fastapi", "nextjs", "express"],
+    type: ToolType.LLAMAHUB,
+    dependencies: [
+      {
+        name: "llama-index-tools-azure-code-interpreter",
+        version: "0.2.0",
+      },
+    ],
+    envVars: [
+      {
+        name: "AZURE_POOL_MANAGEMENT_ENDPOINT",
+        description:
+          "Please follow this guideline to create and get the pool management endpoint: https://learn.microsoft.com/azure/container-apps/sessions?tabs=azure-cli",
+      },
+      {
+        name: TOOL_SYSTEM_PROMPT_ENV_VAR,
+        description: "System prompt for Azure code interpreter tool.",
+        value: `-You are a Python interpreter that can run any python code in a secure environment.
+- The python code runs in a Jupyter notebook. Every time you call the 'interpreter' tool, the python code is executed in a separate cell. 
+- You are given tasks to complete and you run python code to solve them.
+- It's okay to make multiple calls to interpreter tool. If you get an error or the result is not what you expected, you can call the tool again. Don't give up too soon!
+- Plot visualizations using matplotlib or any other visualization library directly in the notebook.
+- You can install any pip package (if it exists) by running a cell with pip install.`,
+      },
+    ],
+  },
+  {
+    display: "Form Filling",
+    name: "form_filling",
+    supportedFrameworks: ["fastapi"],
+    type: ToolType.LOCAL,
+    dependencies: [
+      {
+        name: "pandas",
+        version: "^2.2.3",
+      },
+      {
+        name: "tabulate",
+        version: "^0.9.0",
+      },
+    ],
+  },
+];
+
+export const getTool = (toolName: string): Tool | undefined => {
+  return supportedTools.find((tool) => tool.name === toolName);
+};
+
+export const getTools = (toolsName: string[]): Tool[] => {
+  const tools: Tool[] = [];
+  for (const toolName of toolsName) {
+    const tool = getTool(toolName);
+    if (!tool) {
+      console.log(
+        red(
+          `Error: Tool '${toolName}' is not supported. Supported tools are: ${supportedTools
+            .map((t) => t.name)
+            .join(", ")}`,
+        ),
+      );
+      process.exit(1);
+    }
+    tools.push(tool);
+  }
   return tools;
 };
 
-export const lookupTools = async (
-  toolNames: string[],
-): Promise<BaseToolWithCall[]> => {
-  const availableTools = await getAvailableTools();
-  return availableTools.filter((tool) =>
-    toolNames.includes(tool.metadata.name),
-  );
+export const toolRequiresConfig = (tool: Tool): boolean => {
+  const hasConfig = Object.keys(tool.config || {}).length > 0;
+  const hasEmptyEnvVar = tool.envVars?.some((envVar) => !envVar.value) ?? false;
+  return hasConfig || hasEmptyEnvVar;
 };
 
-/**
- * Call multiple tools and return the tool messages
- */
-export const callTools = async (
-  toolCalls: ToolCall[],
-  tools: BaseToolWithCall[],
-  ctx: Context,
-  agentName: string,
-  writeEvent: boolean = true,
-  // eslint-disable-next-line max-params
-): Promise<ChatMessage[]> => {
-  const toolMsgs: ChatMessage[] = [];
-  if (toolCalls.length === 0) {
-    return toolMsgs;
+export const toolsRequireConfig = (tools?: Tool[]): boolean => {
+  if (tools) {
+    return tools?.some(toolRequiresConfig);
   }
-  if (toolCalls.length === 1) {
-    return [
-      (await callSingleTool(toolCalls[0], tools[0], (msg: string) => {
-        if (writeEvent) {
-          ctx.writeEventToStream(
-            new AgentRunEvent({
-              name: agentName,
-              text: msg,
-              type: "text",
-            }),
-          );
-        }
-      })) as ChatMessage,
-    ];
-  }
-  // Multiple tool calls, show events in progress
-  const progressId = crypto.randomUUID();
-  const totalSteps = toolCalls.length;
-  let currentStep = 0;
-  for (const toolCall of toolCalls) {
-    const tool = tools.find((tool) => tool.metadata.name === toolCall.name);
-    if (!tool) {
-      throw new Error(`Tool ${toolCall.name} not found`);
-    }
-    const toolMsg = await callSingleTool(toolCall, tool, (msg: string) => {
-      ctx.writeEventToStream(
-        new AgentRunEvent({
-          name: agentName,
-          text: msg,
-          type: "progress",
-          data: {
-            id: progressId,
-            total: totalSteps,
-            current: currentStep,
-          },
-        }),
-      );
-      currentStep++;
-    });
-    toolMsgs.push(toolMsg as ChatMessage);
-  }
-  return toolMsgs;
+  return false;
 };
 
-export const callSingleTool = async (
-  toolCall: ToolCall,
-  tool: BaseToolWithCall,
-  eventEmitter: (msg: string) => void,
-) => {
-  if (eventEmitter) {
-    eventEmitter(
-      `Calling tool ${toolCall.name} with input: ${JSON.stringify(toolCall.input)}`,
-    );
-  }
-
-  const toolOutput = await callTool(tool, toolCall, {
-    log: () => {},
-    error: (...args: unknown[]) => {
-      console.error(`Tool ${toolCall.name} got error:`, ...args);
-      if (eventEmitter) {
-        eventEmitter(`Tool ${toolCall.name} got error: ${args.join(" ")}`);
-      }
-      return {
-        content: JSON.stringify({
-          error: args.join(" "),
-        }),
-        role: "user",
-        options: {
-          toolResult: {
-            id: toolCall.id,
-            result: JSON.stringify({
-              error: args.join(" "),
-            }),
-            isError: true,
-          },
-        },
-      };
-    },
-    warn: () => {},
-  });
-
-  return {
-    content: JSON.stringify(toolOutput.output),
-    role: "user",
-    options: {
-      toolResult: {
-        result: toolOutput.output,
-        isError: toolOutput.isError,
-        id: toolCall.id,
-      },
-    },
-  };
-};
-
-class ChatWithToolsResponse {
-  toolCalls: ToolCall[];
-  toolCallMessage?: ChatMessage;
-  responseGenerator?: AsyncGenerator<ChatMessage>;
-
-  constructor(options: {
-    toolCalls: ToolCall[];
-    toolCallMessage?: ChatMessage;
-    responseGenerator?: AsyncGenerator<ChatMessage>;
-  }) {
-    this.toolCalls = options.toolCalls;
-    this.toolCallMessage = options.toolCallMessage;
-    this.responseGenerator = options.responseGenerator;
-  }
-
-  isCallingDifferentTools() {
-    // toolCalls is have different tool names
-    const toolNames = this.toolCalls.map((toolCall) => toolCall.name);
-    const uniqueToolNames = new Set(toolNames);
-    return uniqueToolNames.size > 1;
-  }
-
-  isCallingTool() {
-    return this.toolCalls.length > 0;
-  }
-
-  toolName() {
-    return this.toolCalls[0].name;
-  }
-
-  async asFullResponse() {
-    if (!this.responseGenerator) {
-      throw new Error("No response generator");
-    }
-    let fullResponse = "";
-    for await (const chunk of this.responseGenerator) {
-      fullResponse += chunk.content;
-    }
-    return fullResponse;
-  }
+export enum ConfigFileType {
+  YAML = "yaml",
+  JSON = "json",
 }
 
-export const chatWithTools = async (
-  llm: ToolCallLLM,
-  tools: BaseToolWithCall[],
-  messages: ChatMessage[],
+export const writeToolsConfig = async (
+  root: string,
+  tools: Tool[] = [],
+  type: ConfigFileType = ConfigFileType.YAML,
 ) => {
-  const responseGenerator = async function* () {
-    const responseStream = await llm.chat({ messages, tools, stream: true });
-
-    let fullResponse = null;
-    let yieldedIndicator = false;
-    const toolCallMap = new Map();
-    for await (const chunk of responseStream) {
-      const hasToolCalls = chunk.options && "toolCall" in chunk.options;
-      if (!hasToolCalls) {
-        if (!yieldedIndicator) {
-          yield false;
-          yieldedIndicator = true;
-        }
-        yield chunk;
-      } else if (!yieldedIndicator) {
-        yield true;
-        yieldedIndicator = true;
-      }
-
-      if (chunk.options && "toolCall" in chunk.options) {
-        for (const toolCall of chunk.options.toolCall as PartialToolCall[]) {
-          if (toolCall.id) {
-            toolCallMap.set(toolCall.id, toolCall);
-          }
-        }
-      }
-
-      if (
-        hasToolCalls &&
-        (chunk.raw as any)?.choices?.[0]?.finish_reason !== null
-      ) {
-        // Update the fullResponse with the tool calls
-        const toolCalls = Array.from(toolCallMap.values());
-        fullResponse = {
-          ...chunk,
-          options: {
-            ...chunk.options,
-            toolCall: toolCalls,
-          },
-        } as ChatResponseChunk<ToolCallLLMMessageOptions>;
-      }
-    }
-
-    if (fullResponse) {
-      yield fullResponse;
-    }
+  const configContent: {
+    [key in ToolType]: Record<string, any>;
+  } = {
+    local: {},
+    llamahub: {},
   };
-
-  const generator = responseGenerator();
-  const isToolCall = await generator.next();
-
-  if (isToolCall.value) {
-    // If it's a tool call, we need to wait for the full response
-    let fullResponse = null;
-    for await (const chunk of generator) {
-      fullResponse = chunk;
+  tools.forEach((tool) => {
+    if (tool.type === ToolType.LLAMAHUB) {
+      configContent.llamahub[tool.name] = tool.config ?? {};
     }
-
-    if (fullResponse) {
-      const toolCalls = getToolCallsFromResponse(
-        fullResponse as ChatResponseChunk<ToolCallLLMMessageOptions>,
-      );
-      return new ChatWithToolsResponse({
-        toolCalls,
-        toolCallMessage: fullResponse as unknown as ChatMessage,
-      });
-    } else {
-      throw new Error("Cannot get tool calls from response");
+    if (tool.type === ToolType.LOCAL) {
+      configContent.local[tool.name] = tool.config ?? {};
     }
-  }
-
-  return new ChatWithToolsResponse({
-    toolCalls: [],
-    responseGenerator: generator as AsyncGenerator<ChatMessage>,
   });
-};
-
-export const getToolCallsFromResponse = (
-  response:
-    | ChatResponse<ToolCallLLMMessageOptions>
-    | ChatResponseChunk<ToolCallLLMMessageOptions>,
-): ToolCall[] => {
-  let options;
-
-  if ("message" in response) {
-    options = response.message.options;
+  const configPath = path.join(root, "config");
+  await makeDir(configPath);
+  if (type === ConfigFileType.YAML) {
+    await fs.writeFile(
+      path.join(configPath, "tools.yaml"),
+      yaml.stringify(configContent),
+    );
   } else {
-    options = response.options;
+    await fs.writeFile(
+      path.join(configPath, "tools.json"),
+      JSON.stringify(configContent, null, 2),
+    );
   }
-
-  if (options && "toolCall" in options) {
-    return options.toolCall as ToolCall[];
-  }
-  return [];
 };
