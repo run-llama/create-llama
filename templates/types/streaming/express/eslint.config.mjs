8bf3236db06a6c29c03269c923b6d6dc3d840bc2
Thuc Pham <51660321+thucpn@users.noreply.github.com>
2024-11-13 11:28:30 +0700

    upgrade eslint and add lint script for express

---
+++ templates/types/streaming/express/eslint.config.mjs
@@ -0,0 +1,31 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends("eslint:recommended", "prettier"),
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["prettier.config.cjs"],
+  },
+  { files: ["**/*.{ts}"] },
+  {
+    rules: {
+      "max-params": ["error", 4],
+      "prefer-const": "error",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+];
