fbed55a8d8d44bf14fcbe415475a1c345898fd56
leehuwuj <leehuwuj@gmail.com>
2025-04-25 19:38:04 +0700

    Merge remote-tracking branch 'origin/main' into lee/add-artifact

---
+++ .changeset/plenty-spies-tickle.md
@@ -0,0 +1,5 @@
+---
+"create-llama": patch
+---
+
+chore: create-llama monorepo
--- packages/create-llama/e2e/typescript/resolve_dependencies.spec.ts
+++ packages/create-llama/e2e/typescript/resolve_dependencies.spec.ts
@@ -74,7 +74,7 @@ test.describe("Test resolve TS dependencies", () => {
     // Install dependencies using pnpm
     try {
       const { stderr: installStderr } = await execAsync(
-        "pnpm install --prefer-offline",
+        "pnpm install --prefer-offline --ignore-workspace",
         {
           cwd: appDir,
         },
