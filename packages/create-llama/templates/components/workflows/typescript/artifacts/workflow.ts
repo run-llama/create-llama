2e22741d29bd65fa1266f35fea584f0f881723a2
leehuwuj <leehuwuj@gmail.com>
2025-04-29 11:24:23 +0700

    revert change for TS

--- packages/create-llama/templates/components/workflows/typescript/artifacts/workflow.ts
+++
@@ -1,6 +0,0 @@
-import { Settings } from "llamaindex";
-import { getWorkflow } from "./code-workflow";
-
-export const workflowFactory = async (reqBody: any) => {
-  return getWorkflow(reqBody, Settings.llm);
-};
