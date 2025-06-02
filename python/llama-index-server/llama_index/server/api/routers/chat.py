100d4b2091884af021a21c16284a5b5c4b822634
leehuwuj <leehuwuj@gmail.com>
2025-06-02 10:19:05 +0700

    Refactor workflow handling by introducing HITLWorkflowService

--- python/llama-index-server/llama_index/server/api/routers/chat.py
+++ python/llama-index-server/llama_index/server/api/routers/chat.py
@@ -6,7 +6,6 @@ from typing import AsyncGenerator, Callable, Union
 
 from fastapi import APIRouter, BackgroundTasks, HTTPException
 from fastapi.responses import StreamingResponse
-
 from llama_index.core.agent.workflow.workflow_events import (
     AgentInput,
     AgentSetup,
@@ -25,7 +24,7 @@ from llama_index.server.api.callbacks import (
 )
 from llama_index.server.api.callbacks.stream_handler import StreamHandler
 from llama_index.server.api.utils.vercel_stream import VercelStreamResponse
-from llama_index.server.api.utils.workflow import WorkflowService
+from llama_index.server.api.utils.workflow import HITLWorkflowService
 from llama_index.server.models.chat import ChatRequest
 from llama_index.server.models.hitl import HumanInputEvent
 from llama_index.server.services.llamacloud import LlamaCloudFileService
@@ -59,7 +58,7 @@ def chat_router(
             # Check if we should resume a chat with a human response
             hitl_data = last_message.human_response
             if hitl_data:
-                workflow_handler = await WorkflowService.resume_with_hitl_response(
+                workflow_handler = await HITLWorkflowService.resume_with_hitl_response(
                     workflow, hitl_data, request_id=request.id
                 )
             else:
@@ -150,7 +149,7 @@ async def _stream_content(
                 if ctx is None:
                     raise RuntimeError("Context is None")
                 # Save the context with the HITL event
-                await WorkflowService.save_context(
+                await HITLWorkflowService.save_context(
                     chat_id=chat_id,
                     ctx=ctx,
                     hitl_event=event,
