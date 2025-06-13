79afe240e32f5c78db695205ce0ddd50fb8d1c37
thucpn <thucsh2@gmail.com>
2025-06-13 09:41:03 +0700

    increase timeout

--- packages/create-llama/playwright.config.ts
+++ packages/create-llama/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  timeout: 1000 * 60 * 5,
+  timeout: 1000 * 60 * 10,
   reporter: "html",
   use: {
     trace: "on-first-retry",
