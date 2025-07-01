274222287aa624e0347ae46db1a794cb6fc17a2f
thucpn <thucsh2@gmail.com>
2025-07-01 11:37:59 +0700

    update constants

--- packages/server/next/app/constants.ts
+++ packages/server/next/app/constants.ts
@@ -1,4 +1,11 @@
 export const BASE_PATH = "";
 
-export const LLAMA_LOGO_URL = `${BASE_PATH}/llama.png`;
-export const SCRIPT_PATH = `${BASE_PATH}/config.js`;
+const DEFAULT_LLAMA_LOGO_URL = "/llama.png";
+export const LLAMA_LOGO_URL = BASE_PATH
+  ? `${BASE_PATH}/llama.png`
+  : DEFAULT_LLAMA_LOGO_URL;
+
+const DEFAULT_SCRIPT_PATH = "./config.js";
+export const SCRIPT_PATH = BASE_PATH
+  ? `${BASE_PATH}/config.js`
+  : DEFAULT_SCRIPT_PATH;
