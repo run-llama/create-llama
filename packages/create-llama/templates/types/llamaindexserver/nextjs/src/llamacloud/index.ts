c11ff27314c381b452364610a183d93e2d4178d7
thucpn <thucsh2@gmail.com>
2025-07-14 19:13:25 +0700

    update CL for ts

--- packages/create-llama/templates/types/llamaindexserver/nextjs/src/llamacloud/index.ts
+++ packages/create-llama/templates/types/llamaindexserver/nextjs/src/llamacloud/index.ts
@@ -1,10 +1,15 @@
 import { LlamaIndexServer } from "@llamaindex/server";
+import "dotenv/config";
+import { initSettings } from "./app/settings";
+import { workflowFactory } from "./app/workflow";
+
+initSettings();
 
 new LlamaIndexServer({
+  workflow: workflowFactory,
   uiConfig: {
     componentsDir: "components",
-    layoutDir: "layout",
-    llamaDeploy: { deployment: "chat", workflow: "workflow" },
+    devMode: true,
   },
   llamaCloud: {
     outputDir: "output/llamacloud",
